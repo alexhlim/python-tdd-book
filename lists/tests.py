59df119c4f4dd36f9f1fdf8d5acfcf5dfaa0b9d3
Alex Lim <alexanderheionlim@gmail.com>
2020-05-07 21:56:41 -0400

    Further along functional tests

--- lists/tests.py
+++ lists/tests.py
@@ -12,7 +12,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
 
@@ -21,4 +20,13 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
